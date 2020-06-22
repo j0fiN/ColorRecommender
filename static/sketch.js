35bdff488dd5e62a4ac729026ad78c82fb95d249
j0fiN <jofinfab@gmail.com>
2020-06-22 21:16:27 +0530

    advance Interactivity :sparkles:

--- static/sketch.js
+++ static/sketch.js
@@ -1,7 +1,11 @@
-let r1, r2, r3, b1, b2, b3, g1, g2, g3;
-
+var r1; var g1; var b1;
+var r2; var g2; var b2;
+var r3; var g3; var b3;
+var w1 = 200; var h1 = 400;
+var w2 = 200; var h2 = 400;
+var w3 = 200; var h3 = 400;
 function setup() {
-  createCanvas(900, 700);
+  createCanvas(600, 400);
   r1 = floor(random(0,255));
   r2 = floor(random(0,255));
   r3 = floor(random(0,255));
@@ -18,9 +22,22 @@ function draw() {
   background(255, 255, 255);
   noStroke();
   fill(r1, g1, b1);
-  rect(0, 0, 300, 700);
+  rect(0, 0, w1, h1);
   fill(r2, g2, b2);
-  rect(300,0,300,700);
+  rect(200, 0, w2, h2);
   fill(r3, g3, b3);
-  rect(600,0,300,700);
+  rect(400, 0, w3, h3);
+  textSize(15);
+  if(mouseX > 0 && mouseX < 200 && mouseY > 0 && mouseY < height){
+    fill(0, 0, 0);
+    text((r1)+" , "+(g1) +" , "+(b1),mouseX, mouseY);
+  }
+  if(mouseX > 200 && mouseX < 400 && mouseY > 0 && mouseY < height){
+    fill(0, 0, 0);
+    text((r2)+" , "+(g2) +" , "+(b2),mouseX, mouseY);
+  }
+  if(mouseX > 400 && mouseX < 600 && mouseY > 0 && mouseY < height){
+    fill(0, 0, 0);
+    text((r3)+" , "+(g3) +" , "+(b3), mouseX, mouseY);
+  }
 }
