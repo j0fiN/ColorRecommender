2729ce66ff7381b7b9a27a598a120175b07eb04b
j0fiN <jofinfab@gmail.com>
2020-06-22 14:42:11 +0530

    developments :book:

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 app.config["SECRET_KEY"] = os.getenv("APP_SECRET_KEY")
 socketio = SocketIO(app)
 
-@app.route('/')
-def hello():
-    return render_template("index.html")
+# @app.route('/')
+# def hello():
+#     return render_template("index.html")
 
-@socketio.on('message', namespace="/test")
-def handle(message):
-    send(message,broadcast=True)
+@socketio.on('message')
+def handle(msg):
+    print("hello")
+    send(msg, broadcast=True)
 
 if __name__ == "__main__":
     socketio.run(app)
