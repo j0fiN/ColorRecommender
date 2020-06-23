4474edd951f07c1dbab187791a5fd6b58986d35c
j0fiN <jofinfab@gmail.com>
2020-06-23 16:05:23 +0530

    simple Model test

---
+++ Tests/MLTest.py
@@ -0,0 +1,25 @@
+import unittest
+from ColorPickerModel import ML_Model
+
+
+class TestCase(unittest.TestCase):
+
+    def test_1(self):
+        m = ML_Model()
+        self.assertEqual(200, m.res.status_code)
+
+    def test_2(self):
+        m = ML_Model()
+        self.assertEqual(len(m.predictor(0, 0, 0)), 1)
+
+    def test_3(self):
+        m = ML_Model()
+        self.assertEqual(m.preprocess(), None)
+
+    def test_4(self):
+        m = ML_Model()
+        self.assertIsNotNone(m.train())
+
+
+if __name__ == '__main__':
+    unittest.main()
