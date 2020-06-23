f1f4f2d939bc8a703284a3f320635a8d7b60b761
j0fiN <jofinfab@gmail.com>
2020-06-23 10:10:58 +0530

    Major Developements :sparkles:

--- static/sketch.js
+++ static/sketch.js
@@ -5,8 +5,9 @@ var w1 = 200; var h1 = 400;
 var w2 = 200; var h2 = 400;
 var w3 = 200; var h3 = 400;
 var Width = 600; var Height = 400;
+var ratings;
 function setup() {
-  let cnv = createCanvas(Width, Height);
+  let cnv = createCanvas(Width, 600);
   cnv.position(50, 100);
   r1 = floor(random(0,255));
   r2 = floor(random(0,255));
@@ -17,11 +18,12 @@ function setup() {
   b1 = floor(random(0,255));
   b2 = floor(random(0,255));
   b3 = floor(random(0,255));
+  ratings = floor(random(1,3));
 
 }
 
 function draw() {
-    background(0, 0, 0);
+    background(255, 255, 255);
     noStroke();
     fill(r1, g1, b1);
     rect(0, 0, Width/3, Height);
@@ -30,43 +32,47 @@ function draw() {
     fill(r3, g3, b3);
     rect(2*Width/3, 0, Width/3, Height);
     textSize(15);
-    if (mouseX > 0 && mouseX < 200 && mouseY > 0 && mouseY < height) {
+    if (mouseX > 0 && mouseX < 200 && mouseY > 0 && mouseY < Height) {
       fill(0, 0, 0);
       text("RGB: " + (r1) + ", " + (g1) + ", " + (b1), mouseX, mouseY);
     }
-    if (mouseX > 200 && mouseX < 400 && mouseY > 0 && mouseY < height) {
+    if (mouseX > 200 && mouseX < 400 && mouseY > 0 && mouseY < Height) {
       fill(0, 0, 0);
       text("RGB: " + (r2) + ", " + (g2) + ", " + (b2), mouseX, mouseY);
     }
-    if (mouseX > 400 && mouseX < 600 && mouseY > 0 && mouseY < height) {
+    if (mouseX > 400 && mouseX < 600 && mouseY > 0 && mouseY < Height) {
       fill(0, 0, 0);
       text("RGB: " + (r3) + ", " + (g3) + ", " + (b3), mouseX, mouseY);
     }
+    fill(0,0,0);
+    textSize(30);
+    text(ratings, 300, 0);
 
 }
 
 function mouseClicked() {
   let values;
-    if (mouseX > 0 && mouseX < 200 && mouseY > 0 && mouseY < height) {
-      values = {r: r1, g: g1, b: b1};
+    if (mouseX > 0 && mouseX < 200 && mouseY > 0 && mouseY < Height) {
+      values = {r: r1, g: g1, b: b1, rate:ratings};
       r1 = floor(random(0, 255));
       g1 = floor(random(0, 255));
       b1 = floor(random(0, 255));
+      ratings = floor(random(1,3));
     }
-    if (mouseX > 200 && mouseX < 400 && mouseY > 0 && mouseY < height) {
-      values = {r: r2, g: g2, b: b2};
+    if (mouseX > 200 && mouseX < 400 && mouseY > 0 && mouseY < Height) {
+      values = {r: r2, g: g2, b: b2, rate:ratings};
       r2 = floor(random(0, 255));
       g2 = floor(random(0, 255));
       b2 = floor(random(0, 255));
+      ratings = floor(random(1,3));
     }
-    if (mouseX > 400 && mouseX < 600 && mouseY > 0 && mouseY < height) {
-      values = {r: r3, g: g3, b: b3};
+    if (mouseX > 400 && mouseX < 600 && mouseY > 0 && mouseY < Height) {
+      values = {r: r3, g: g3, b: b3, rate:ratings};
       r3 = floor(random(0, 255));
       g3 = floor(random(0, 255));
       b3 = floor(random(0, 255));
+      ratings = floor(random(1,3));
     }
-    // console.log(typeof values);
-    // console.log(JSON.stringify(values));
     fetch('http://127.0.0.1:5000/api', {
       method: 'POST',
       body: JSON.stringify(values),
